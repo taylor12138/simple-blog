32cb95b2275f62b1ab00b01fdeaf2fb4c59a8db5
linzizhan <linzizhan@corp.netease.com>
2025-04-23 14:27:16 +0800

    fix: site

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,8 +29,10 @@ export default defineConfig({
                 "@": "/src",
             },
         },
+        base: process.env.GITHUB_ACTIONS ? "/simple-blog/" : "/",
     },
-    site: "https://blog.plr.moe",
+    site: process.env.GITHUB_ACTIONS ? "https://taylor12138.github.io/simple-blog/" : "https://blog.plr.moe",
+    base: process.env.GITHUB_ACTIONS ? "/simple-blog" : "",
     integrations: [
         mdx(),
         sitemap(),
